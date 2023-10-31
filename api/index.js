359f9fa304bb3f71193569891f2c4e21511f0fe8
AbdullahBahttab <aaa.bahttab@gmail.com>
2023-11-01 00:47:37 +0300

    connect db

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log('connected to mongodb');
+})
+.catch
+((err)=>{console.error(err)});
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
